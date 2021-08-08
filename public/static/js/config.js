7436591fdf53166c385bc8588c9c07f17482f81b
lian <duanliandba@gmail.com>
2021-08-08 08:32:19 -0400

    Update

--- public/static/js/config.js
+++ public/static/js/config.js
@@ -1,4 +1,4 @@
 window.g = {
-  // url: 'http://192.168.38.125:9999' 
-    url: 'http://api.helm3training.com' // API server address
+   url: 'http://localhost:9999' 
+  //  url: 'http://api.helm3training.com' // API server address
   }
