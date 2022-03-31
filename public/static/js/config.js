5870938b961f6f33db4f323116e7c69ffe68b8a7
lian <duanliandba@gmail.com>
2022-03-31 05:45:44 -0400

    update
    Signed-off-by: lian <duanliandba@gmail.com>

--- public/static/js/config.js
+++ public/static/js/config.js
@@ -1,4 +1,4 @@
 window.g = {
-  // url: 'http://localhost:9999' 
-   url: 'http://api.helm3training.com' // API server address
+   url: 'http://localhost:9999' 
+  // url: 'http://api.helm3training.com' // API server address
   }
